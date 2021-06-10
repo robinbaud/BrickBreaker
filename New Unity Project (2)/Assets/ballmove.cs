1c289221821b7264dbd9035f8691a809686b0ce3
robin baud <robin.baud@gmail.com>
2021-06-11 00:19:55 +0200

    oui

---
+++ New Unity Project (2)/Assets/ballmove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballmove : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public bool InPlay;
+    public Transform paddle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+        rb.AddForce(Vector2.up * 500);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(InPlay == false){
+            transform.position = paddle.position;
+        }
+        if(Input.GetButtonDown("Jump") && !InPlay) {
+            InPlay = true;
+            rb.AddForce(Vector2.up * 500);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("bottom")){
+            Debug.Log("we hit the bottom");
+            rb.velocity = Vector2.zero;
+            InPlay = false;
+        }
+    }
+}
---
+++ New Unity Project (2)/Assets/paddlemovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class paddlemovement : MonoBehaviour
+{
+    public float speed;
+    public float ledge;
+    public float redge;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Translate(Vector2.right * horizontal * Time.deltaTime * speed);
+        if(transform.position.x < ledge)
+            transform.position = new Vector2(ledge, transform.position.y);
+        if(transform.position.x > redge)
+            transform.position = new Vector2(redge, transform.position.y);
+    }  
+}
